b57841bd4ac1c4953b66e1628a0afc07177b55dc
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2019-08-24 14:55:30 +0200

    feat: setup build with shared collection keys

---
+++ FirestoreCollectionKeys.ts
@@ -0,0 +1,5 @@
+export namespace FirestoreCollectionKeys {
+    export const ACCOUNTS_COLLECTION_KEY = "accounts";
+    export const ADVICES_COLLECTION_KEY = "ahpaa_advices";
+    export const SENT_SMS_MESSAGES_COLLECTION_KEY = "sent_sms_messages";
+}
