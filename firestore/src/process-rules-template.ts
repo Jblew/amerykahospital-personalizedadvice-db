981635510a4563da9ad1b49377b5b271f6cb37a5
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2019-08-25 13:12:27 +0200

    ci: template rules file

---
+++ firestore/src/process-rules-template.ts
@@ -0,0 +1,43 @@
+// tslint:disable no-console
+import * as fs from "fs";
+
+import { FirestoreCollectionKeys, RoleKey } from "../../orm/src/context_for_rules";
+/**
+ *
+ * Variables
+ */
+
+const definedVariables: { [x: string]: string } = Object.freeze({
+    role_medicalprofessional: RoleKey.medicalprofessional,
+    collection_advice: FirestoreCollectionKeys.ADVICES_COLLECTION_KEY,
+    collection_advice_demo: FirestoreCollectionKeys.DEMO_ADVICES_COLLECTION_KEY,
+});
+
+/**
+ *
+ * Processing
+ */
+const data = fs.readFileSync(0, "utf-8");
+
+const matchedExprs = data.match(/§([^§]+)§/gm);
+if (!matchedExprs) errorExit("No variables found");
+
+let output: string = data;
+for (const variableExpr of matchedExprs!) {
+    const variable = variableExpr.substring(1, variableExpr.length - 1);
+    output = replaceVariable(output, variable);
+}
+console.log(output);
+
+//
+function replaceVariable(input: string, variable: string): string {
+    if (!definedVariables.hasOwnProperty(variable)) {
+        errorExit(`Variable ${variable} is not defined`);
+    }
+    return input.replace("§" + variable + "§", definedVariables[variable]);
+}
+
+function errorExit(msg: string) {
+    console.error(msg);
+    process.exit(1);
+}
--- orm/src/config/FirestoreCollectionKeys.ts
+++ orm/src/config/FirestoreCollectionKeys.ts
@@ -1,5 +1,6 @@
 export namespace FirestoreCollectionKeys {
     export const ACCOUNTS_COLLECTION_KEY = "accounts";
     export const ADVICES_COLLECTION_KEY = "ahpaa_advices";
+    export const DEMO_ADVICES_COLLECTION_KEY = "ahpaa_advices_demo";
     export const SENT_SMS_MESSAGES_COLLECTION_KEY = "sent_sms_messages";
 }
